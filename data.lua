682559378374d07cea58afb7d4d7111498d5e4b0
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-06-13 01:08:33 -0500

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,4 +1,5 @@
 require("prototypes.entities")
 require("prototypes.items")
+-- if Krastorio2, then data_updates will require recipes again
 require("prototypes.recipes")
 require("prototypes.technology")
