06128b6c2a1e467a532d5a9979a59a3fb0d1be97
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-06-28 19:45:01 -0500

    Fix K2 extended length for fast and express

--- data-updates.lua
+++
@@ -1,4 +0,0 @@
--- Krastorio2 changes the vanilla underground belt recipes
-if mods["Krastorio2"] then
-    require("prototypes.recipes")
-end
--- data.lua
+++
@@ -1,5 +0,0 @@
-require("prototypes.entities")
-require("prototypes.items")
--- if Krastorio2, then data-updates will require recipes again
-require("prototypes.recipes")
-require("prototypes.technology")
