d02b24c499d31d82a4edb2fff1b0896b7acabf20
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-07-02 21:52:41 -0500

    Refactoring

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -2,60 +2,52 @@ local braidy_belt = table.deepcopy(data.raw["underground-belt"]["underground-bel
 braidy_belt.name = "braidy-belt"
 braidy_belt.minable.result = "braidy-belt"
 braidy_belt.next_upgrade = "fast-braidy-belt"
-braidy_belt.order = data.raw["item"]["underground-belt"].order .. "z"
 
 local fast_braidy_belt = table.deepcopy(data.raw["underground-belt"]["fast-underground-belt"])
 fast_braidy_belt.name = "fast-braidy-belt"
 fast_braidy_belt.minable.result = "fast-braidy-belt"
 fast_braidy_belt.next_upgrade = "express-braidy-belt"
-fast_braidy_belt.order = data.raw["item"]["fast-underground-belt"].order .. "z"
 
 local express_braidy_belt = table.deepcopy(data.raw["underground-belt"]["express-underground-belt"])
 express_braidy_belt.name = "express-braidy-belt"
 express_braidy_belt.minable.result = "express-braidy-belt"
 express_braidy_belt.next_upgrade = nil
-express_braidy_belt.order = data.raw["item"]["express-underground-belt"].order .. "z"
+
+data:extend {braidy_belt, fast_braidy_belt, express_braidy_belt}
 
 if mods["Krastorio2"] then
-    express_braidy_belt.next_upgrade = "advanced-braidy-belt"
+    data.raw["underground-belt"]["express-braidy-belt"].next_upgrade = "advanced-braidy-belt"
 
     local advanced_braidy_belt = table.deepcopy(data.raw["underground-belt"]["kr-advanced-underground-belt"])
     advanced_braidy_belt.name = "advanced-braidy-belt"
     advanced_braidy_belt.minable.result = "advanced-braidy-belt"
     advanced_braidy_belt.next_upgrade = "superior-braidy-belt"
-    advanced_braidy_belt.order = data.raw["item"]["kr-advanced-underground-belt"].order .. "z"
 
     local superior_braidy_belt = table.deepcopy(data.raw["underground-belt"]["kr-superior-underground-belt"])
     superior_braidy_belt.name = "superior-braidy-belt"
     superior_braidy_belt.minable.result = "superior-braidy-belt"
     superior_braidy_belt.next_upgrade = nil
-    superior_braidy_belt.order = data.raw["item"]["kr-superior-underground-belt"].order .. "z"
 
     data:extend {advanced_braidy_belt, superior_braidy_belt}
 end
 
 if mods["AdvancedBelts"] then
-    express_braidy_belt.next_upgrade = "extreme-braidy-belt"
+    data.raw["underground-belt"]["express-braidy-belt"].next_upgrade = "extreme-braidy-belt"
 
     local extreme_braidy_belt = table.deepcopy(data.raw["underground-belt"]["extreme-underground"])
     extreme_braidy_belt.name = "extreme-braidy-belt"
     extreme_braidy_belt.minable.result = "extreme-braidy-belt"
     extreme_braidy_belt.next_upgrade = "ultimate-braidy-belt"
-    extreme_braidy_belt.order = data.raw["item"]["extreme-underground"].order .. "z"
 
     local ultimate_braidy_belt = table.deepcopy(data.raw["underground-belt"]["ultimate-underground"])
     ultimate_braidy_belt.name = "ultimate-braidy-belt"
     ultimate_braidy_belt.minable.result = "ultimate-braidy-belt"
     ultimate_braidy_belt.next_upgrade = "high-speed-braidy-belt"
-    ultimate_braidy_belt.order = data.raw["item"]["ultimate-underground"].order .. "z"
 
     local high_speed_braidy_belt = table.deepcopy(data.raw["underground-belt"]["high-speed-underground"])
     high_speed_braidy_belt.name = "high-speed-braidy-belt"
     high_speed_braidy_belt.minable.result = "high-speed-braidy-belt"
     high_speed_braidy_belt.next_upgrade = nil
-    high_speed_braidy_belt.order = data.raw["item"]["high-speed-underground"].order .. "z"
 
     data:extend {extreme_braidy_belt, ultimate_braidy_belt, high_speed_braidy_belt}
 end
-
-data:extend {braidy_belt, fast_braidy_belt, express_braidy_belt}
--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -1,10 +1,13 @@
+local conversion_recipe = (settings.startup["bb-recipe-type"].value == "conversion")
+
 local braidy_belt = table.deepcopy(data.raw["recipe"]["underground-belt"])
 braidy_belt.name = "braidy-belt"
 braidy_belt.result = "braidy-belt"
-if settings.startup["bb-recipe-type"].value == "conversion" then
+if conversion_recipe then
     braidy_belt.ingredients = {{"underground-belt", 2}}
 end
 
+-- aai-industry seems to envoke the expensive recipe
 if mods["aai-industry"] then
     if braidy_belt.expensive then
         for _, result in pairs(braidy_belt.expensive.results) do
@@ -12,7 +15,7 @@ if mods["aai-industry"] then
                 result.name = "braidy-belt"
             end
         end
-        if settings.startup["bb-recipe-type"].value == "conversion" then
+        if conversion_recipe then
             braidy_belt.expensive.ingredients = {{"underground-belt", 2}}
         end
     end
@@ -23,7 +26,7 @@ if mods["aai-industry"] then
                 result.name = "braidy-belt"
             end
         end
-        if settings.startup["bb-recipe-type"].value == "conversion" then
+        if conversion_recipe then
             braidy_belt.normal.ingredients = {{"underground-belt", 2}}
         end
     end
@@ -32,14 +35,14 @@ end
 local fast_braidy_belt = table.deepcopy(data.raw["recipe"]["fast-underground-belt"])
 fast_braidy_belt.name = "fast-braidy-belt"
 fast_braidy_belt.result = "fast-braidy-belt"
-if settings.startup["bb-recipe-type"].value == "conversion" then
+if conversion_recipe then
     fast_braidy_belt.ingredients = {{"fast-underground-belt", 2}}
 end
 
 local express_braidy_belt = table.deepcopy(data.raw["recipe"]["express-underground-belt"])
 express_braidy_belt.name = "express-braidy-belt"
 express_braidy_belt.result = "express-braidy-belt"
-if settings.startup["bb-recipe-type"].value == "conversion" then
+if conversion_recipe then
     express_braidy_belt.ingredients = {{"express-underground-belt", 2}}
 end
 
@@ -49,14 +52,14 @@ if mods["Krastorio2"] then
     local advanced_braidy_belt = table.deepcopy(data.raw["recipe"]["kr-advanced-underground-belt"])
     advanced_braidy_belt.name = "advanced-braidy-belt"
     advanced_braidy_belt.result = "advanced-braidy-belt"
-    if settings.startup["bb-recipe-type"].value == "conversion" then
+    if conversion_recipe then
         advanced_braidy_belt.ingredients = {{"kr-advanced-underground-belt", 2}}
     end
 
     local superior_braidy_belt = table.deepcopy(data.raw["recipe"]["kr-superior-underground-belt"])
     superior_braidy_belt.name = "superior-braidy-belt"
     superior_braidy_belt.result = "superior-braidy-belt"
-    if settings.startup["bb-recipe-type"].value == "conversion" then
+    if conversion_recipe then
         superior_braidy_belt.ingredients = {{"kr-superior-underground-belt", 2}}
     end
 
@@ -67,21 +70,21 @@ if mods["AdvancedBelts"] then
     local extreme_braidy_belt = table.deepcopy(data.raw["recipe"]["extreme-underground"])
     extreme_braidy_belt.name = "extreme-braidy-belt"
     extreme_braidy_belt.result = "extreme-braidy-belt"
-    if settings.startup["bb-recipe-type"].value == "conversion" then
+    if conversion_recipe then
         extreme_braidy_belt.ingredients = {{"extreme-underground", 2}}
     end
 
     local ultimate_braidy_belt = table.deepcopy(data.raw["recipe"]["ultimate-underground"])
     ultimate_braidy_belt.name = "ultimate-braidy-belt"
     ultimate_braidy_belt.result = "ultimate-braidy-belt"
-    if settings.startup["bb-recipe-type"].value == "conversion" then
+    if conversion_recipe then
         ultimate_braidy_belt.ingredients = {{"ultimate-underground", 2}}
     end
 
     local high_speed_braidy_belt = table.deepcopy(data.raw["recipe"]["high-speed-underground"])
     high_speed_braidy_belt.name = "high-speed-braidy-belt"
     high_speed_braidy_belt.result = "high-speed-braidy-belt"
-    if settings.startup["bb-recipe-type"].value == "conversion" then
+    if conversion_recipe then
         high_speed_braidy_belt.ingredients = {{"high-speed-underground", 2}}
     end
 
