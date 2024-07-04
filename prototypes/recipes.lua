40a2d80edccd0901680716021f776e0852a8333c
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-07-03 23:56:44 -0500

    Update recipes.lua

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -1,92 +1,70 @@
 local conversion_recipe = (settings.startup["bb-recipe-type"].value == "conversion")
 
-local braidy_belt = table.deepcopy(data.raw["recipe"]["underground-belt"])
-braidy_belt.name = "braidy-belt"
-braidy_belt.result = "braidy-belt"
-if conversion_recipe then
-    braidy_belt.ingredients = {{"underground-belt", 2}}
+local function clone_recipe(clone_name, original)
+    local clone = table.deepcopy(data.raw["recipe"][original])
+    clone.name = clone_name
+    clone.result = clone_name
+    return clone
 end
 
--- aai-industry seems to envoke the expensive recipe
-if mods["aai-industry"] then
-    if braidy_belt.expensive then
-        for _, result in pairs(braidy_belt.expensive.results) do
+local function adjust_recipe(recipe, ingredient)
+    if conversion_recipe then
+        recipe.ingredients = {{ingredient, 2}}
+    end
+
+    if recipe.expensive then
+        for _, result in pairs(recipe.expensive.results) do
             if result.name then
-                result.name = "braidy-belt"
+                result.name = recipe.name
             end
         end
         if conversion_recipe then
-            braidy_belt.expensive.ingredients = {{"underground-belt", 2}}
+            recipe.expensive.ingredients = {{ingredient, 2}}
         end
     end
 
-    if braidy_belt.normal then
-        for _, result in pairs(braidy_belt.normal.results) do
+    if recipe.normal then
+        for _, result in pairs(recipe.normal.results) do
             if result.name then
-                result.name = "braidy-belt"
+                result.name = recipe.name
             end
         end
         if conversion_recipe then
-            braidy_belt.normal.ingredients = {{"underground-belt", 2}}
+            recipe.normal.ingredients = {{ingredient, 2}}
         end
     end
 end
 
-local fast_braidy_belt = table.deepcopy(data.raw["recipe"]["fast-underground-belt"])
-fast_braidy_belt.name = "fast-braidy-belt"
-fast_braidy_belt.result = "fast-braidy-belt"
-if conversion_recipe then
-    fast_braidy_belt.ingredients = {{"fast-underground-belt", 2}}
-end
+local braidy_belt = clone_recipe("braidy-belt", "underground-belt")
+adjust_recipe(braidy_belt, "underground-belt")
 
-local express_braidy_belt = table.deepcopy(data.raw["recipe"]["express-underground-belt"])
-express_braidy_belt.name = "express-braidy-belt"
-express_braidy_belt.result = "express-braidy-belt"
-if conversion_recipe then
-    express_braidy_belt.ingredients = {{"express-underground-belt", 2}}
-end
+local fast_braidy_belt = clone_recipe("fast-braidy-belt", "fast-underground-belt")
+adjust_recipe(fast_braidy_belt, "fast-underground-belt")
+
+local express_braidy_belt = clone_recipe("express-braidy-belt", "express-underground-belt")
+adjust_recipe(express_braidy_belt, "express-underground-belt")
 
 data:extend {braidy_belt, fast_braidy_belt, express_braidy_belt}
 
 if mods["Krastorio2"] then
-    local advanced_braidy_belt = table.deepcopy(data.raw["recipe"]["kr-advanced-underground-belt"])
-    advanced_braidy_belt.name = "advanced-braidy-belt"
-    advanced_braidy_belt.result = "advanced-braidy-belt"
-    if conversion_recipe then
-        advanced_braidy_belt.ingredients = {{"kr-advanced-underground-belt", 2}}
-    end
+    local advanced_braidy_belt = clone_recipe("advanced-braidy-belt", "kr-advanced-underground-belt")
+    adjust_recipe(advanced_braidy_belt, "kr-advanced-underground-belt")
 
-    local superior_braidy_belt = table.deepcopy(data.raw["recipe"]["kr-superior-underground-belt"])
-    superior_braidy_belt.name = "superior-braidy-belt"
-    superior_braidy_belt.result = "superior-braidy-belt"
-    if conversion_recipe then
-        superior_braidy_belt.ingredients = {{"kr-superior-underground-belt", 2}}
-    end
+    local superior_braidy_belt = clone_recipe("superior-braidy-belt", "kr-superior-underground-belt")
+    adjust_recipe(superior_braidy_belt, "kr-superior-underground-belt")
 
     data:extend {advanced_braidy_belt, superior_braidy_belt}
 end
 
 if mods["AdvancedBelts"] then
-    local extreme_braidy_belt = table.deepcopy(data.raw["recipe"]["extreme-underground"])
-    extreme_braidy_belt.name = "extreme-braidy-belt"
-    extreme_braidy_belt.result = "extreme-braidy-belt"
-    if conversion_recipe then
-        extreme_braidy_belt.ingredients = {{"extreme-underground", 2}}
-    end
+    local extreme_braidy_belt = clone_recipe("extreme-braidy-belt", "extreme-underground")
+    adjust_recipe(extreme_braidy_belt, "extreme-underground")
 
-    local ultimate_braidy_belt = table.deepcopy(data.raw["recipe"]["ultimate-underground"])
-    ultimate_braidy_belt.name = "ultimate-braidy-belt"
-    ultimate_braidy_belt.result = "ultimate-braidy-belt"
-    if conversion_recipe then
-        ultimate_braidy_belt.ingredients = {{"ultimate-underground", 2}}
-    end
+    local ultimate_braidy_belt = clone_recipe("ultimate-braidy-belt", "ultimate-underground")
+    adjust_recipe(ultimate_braidy_belt, "ultimate-underground")
 
-    local high_speed_braidy_belt = table.deepcopy(data.raw["recipe"]["high-speed-underground"])
-    high_speed_braidy_belt.name = "high-speed-braidy-belt"
-    high_speed_braidy_belt.result = "high-speed-braidy-belt"
-    if conversion_recipe then
-        high_speed_braidy_belt.ingredients = {{"high-speed-underground", 2}}
-    end
+    local high_speed_braidy_belt = clone_recipe("high-speed-braidy-belt", "high-speed-underground")
+    adjust_recipe(high_speed_braidy_belt, "high-speed-underground")
 
-   data:extend {extreme_braidy_belt, ultimate_braidy_belt, high_speed_braidy_belt}
+    data:extend {extreme_braidy_belt, ultimate_braidy_belt, high_speed_braidy_belt}
 end
