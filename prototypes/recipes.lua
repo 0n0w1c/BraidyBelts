e8af9a201ecbf242bdf9bef890a3fa7d7e5d3268
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-07-06 13:15:30 -0500

    Update recipes.lua

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -62,15 +62,14 @@ local function clone_recipe(clone_name, original)
     local clone = table.deepcopy(data.raw["recipe"][original])
     clone.name = clone_name
     clone.result = clone_name
-    if conversion_recipe then
-        set_conversion(clone, original)
-    else
-        if clone.ingredients then
-            swap_ingredient(clone)
-        end
-    end
     if clone.expensive then
         expensive_mode(clone, original)
+    else
+        if conversion_recipe then
+            set_conversion(clone, original)
+        else
+            swap_ingredient(clone)
+        end
     end
     return clone
 end
